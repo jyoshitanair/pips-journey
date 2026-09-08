80eaaa7562b6831949c28872479470e3b4e7bd12
Jyoshita <jyoshitanair123@gmail.com>
2026-09-07 18:52:02 -0700

    next works not skip

---
+++ src/game.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from 'react'
+export default function Game() {
+    return(
+        <div>
+            <h1> game! </h1>
+        </div>
+    );
+}
--- src/login.jsx
+++ src/login.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react'
 import { motion} from 'framer-motion'
+import Game from './game.jsx'
 import './index.css'
-const all_dialog = ["many millena ago, in a land far away, the local piplings faced a great threat.", "dos mios!"]
+const all_dialog = ["many millena ago, in a land far away, the local piplings faced a great threat.", "dos mios! hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"]
 export default function Login() {
   const [dialogDone, setDialogDone] = useState(false);
-  const [rawr, setRawr] = useState("skip");
+  const [rawr, setRawr] = useState("ah");
+  const [curIndex, setCurIndex] = useState(0);
+  const [page, setPage] = useState("login");
   //
   useEffect(() => {
     const keydown = (e) =>{
@@ -12,39 +15,64 @@ export default function Login() {
         if(!dialogDone){
           setRawr("skip");
         }else{
-          setRawr("next");
+          //move to next dialog
+          if(curIndex < all_dialog.length -1){
+            setCurIndex(prev => prev +1);
+            setRawr("next");
+            setDialogDone(false);
+          }else{
+            setRawr("end");
+            setPage("game");
+
+          }
         }
       }
    }
       window.addEventListener("keydown", keydown);
 
       return () => window.removeEventListener("keydown", keydown);
-  }, [dialogDone]);
+  }, [dialogDone, curIndex]);
 
   return(
     <div>
-      <h1> pip's journey </h1>
-      <div className = "dialog">
-        {dialog("many millena ago, in a land far away, the local piplings faced a great threat.")}
-      </div>
-      <h1> {rawr}</h1>
-
+      {page === "login" && 
+        <div>
+          <h1> pip's journey </h1>
+          <div className = "dialog">
+            <Dialog 
+            key = {curIndex}
+            isSkipped = {rawr === "skip"}
+            sentence = {all_dialog[curIndex]}
+            onComplete = {() => setDialogDone(true)}
+            />
+          </div>
+          <h1> {rawr}</h1>
+        </div>
+      }
+      {page === "game" && <Game/>}
     </div>
+
   );
 }
 
-function dialog(sentence){
-    setDialogDone(false);
+function Dialog({sentence, isSkipped,onComplete}){
       const letters = Array.from(sentence) ///to letters
+      if (isSkipped){
+        return(
+              <motion.span>
+                  <h3> {sentence} </h3>
+              </motion.span>
+          );
+      }
       return letters.map((letter, index) => {
           const scrollsettings = {
-              initial: {opacity: 0, y:-1},
+              initial: {opacity: isSkipped ? 1:0, y:isSkipped ? 0:-1},
               whileInView: {opacity:1, y:0},
               viewport: {once:true},
-              transition: {duration: 0.3, delay: (0.7 + index*0.1)},
+              transition: {duration: isSkipped ? 0:0.07, delay: isSkipped ? 0:(0.07 + index*0.02)},
               onAnimationComplete: () => {
                 if(index === letters.length -1){
-                  setDialogDone(true);
+                  onComplete();
                 }
               }
           }
